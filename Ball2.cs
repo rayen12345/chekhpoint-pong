0464b49e199379d7f7d15105c26d52cae330ffd2
rayen12345 <54586143+rayen12345@users.noreply.github.com>
2019-09-03 11:20:04 +0100

    Add files via upload

---
+++ Ball2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 20;
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketHeight)
+    {
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "right racket")
+        {
+            float y = hitFactor(transform.position,
+                               col.transform.position,
+                               col.collider.bounds.size.y);
+            Vector2 dir = new Vector2(1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+        if (col.gameObject.name == "left racket")
+        {
+            float y = hitFactor(transform.position,
+                               col.transform.position,
+                               col.collider.bounds.size.y);
+            Vector2 dir = new Vector2(1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+
+        void Start()
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Move2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{ public float speed = 30;
+    public string  Axis;
+    void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw(Axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+  
+}
